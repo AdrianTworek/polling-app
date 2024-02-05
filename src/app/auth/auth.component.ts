e21fe087e2a802d71d17d3adaef71f9b214708a6
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-02-05 17:04:31 +0100

    feat: update routes, add Auth and Dashboard components

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { AuthService } from './auth.service';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-auth',
+  standalone: true,
+  imports: [ButtonModule],
+  templateUrl: './auth.component.html',
+})
+export class AuthComponent {
+  private authService = inject(AuthService);
+  private messageService = inject(MessageService);
+  private router = inject(Router);
+
+  onSignUpWithGoogle() {
+    this.authService
+      .signUpWithGoogle()
+      .then(() => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successfully logged in',
+        });
+        this.router.navigate(['/dashboard']);
+      })
+      .catch(() => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Something went wrong',
+          detail: 'Please try again',
+        });
+      });
+  }
+}
