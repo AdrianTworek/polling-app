035a09598b43b6510d12f0878d1e6dc640d16d42
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-02-05 17:02:48 +0100

    feat: AuthService with Google sign up and storing users in Firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
@@ -9,8 +11,10 @@ import { environment } from '../environments/environment';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideAnimations(),
     importProvidersFrom([
       provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+      provideAuth(() => getAuth()),
       provideFirestore(() => getFirestore()),
     ]),
   ],
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,68 @@
+import { Injectable, computed, inject, signal } from '@angular/core';
+import {
+  Auth,
+  GoogleAuthProvider,
+  User,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from '@angular/fire/auth';
+import {
+  Firestore,
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+} from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private auth = inject(Auth);
+  private db = inject(Firestore);
+
+  currentUserSig = signal<User | null>(null);
+  isAuthenticated = computed(() => !!this.currentUserSig());
+
+  constructor() {
+    onAuthStateChanged(this.auth, (user) => {
+      this.currentUserSig.set(user);
+    });
+  }
+
+  async signUpWithGoogle() {
+    const provider = new GoogleAuthProvider();
+
+    return signInWithPopup(this.auth, provider)
+      .then(async (result) => {
+        const user = result.user;
+
+        this.currentUserSig.set(user);
+
+        const q = query(
+          collection(this.db, 'users'),
+          where('uid', '==', user.uid)
+        );
+        const docs = await getDocs(q);
+
+        if (docs.docs.length === 0) {
+          await addDoc(collection(this.db, 'users'), {
+            uid: user.uid,
+            name: user.displayName,
+            authProvider: 'google',
+            email: user.email,
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        this.currentUserSig.set(null);
+      });
+  }
+
+  logOut() {
+    return signOut(this.auth);
+  }
+}
