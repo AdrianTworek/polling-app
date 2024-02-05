a494f562e816b6c28d512286edb39d82360d37b1
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-02-05 17:05:14 +0100

    feat: add Navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, inject } from '@angular/core';
+
+import { MenuItem, MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { AvatarModule } from 'primeng/avatar';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [ButtonModule, MenuModule, AvatarModule],
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent implements OnInit {
+  items: MenuItem[] | undefined;
+  authService = inject(AuthService);
+  messageService = inject(MessageService);
+  router = inject(Router);
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Dashboard',
+        items: [
+          {
+            label: 'Polls',
+            icon: 'pi pi-chart-bar',
+            routerLink: '/dashboard',
+          },
+        ],
+      },
+      {
+        label: 'Account',
+        items: [
+          {
+            label: 'Log out',
+            icon: 'pi pi-sign-out',
+            command: () => {
+              this.authService.logOut().then(() => {
+                this.messageService.add({
+                  severity: 'success',
+                  summary: 'Successfully logged out',
+                  detail: 'See you soon',
+                });
+                this.router.navigate(['/auth']);
+              });
+            },
+          },
+        ],
+      },
+    ];
+  }
+}
