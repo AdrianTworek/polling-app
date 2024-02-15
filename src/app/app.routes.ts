e689b5c064f07a95b679fef568c843a8369ab205
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-02-15 23:38:43 +0100

    feat: add Auth guards in dashboard and login pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,25 @@
 import { Routes } from '@angular/router';
+import {
+  canActivate,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
+
 import { AuthComponent } from './auth/auth.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['auth']);
+const redirectLoggedInToDashboard = () => redirectLoggedInTo(['dashboard']);
 
 export const routes: Routes = [
   {
     path: 'auth',
     component: AuthComponent,
+    ...canActivate(redirectLoggedInToDashboard),
   },
   {
     path: 'dashboard',
     component: DashboardComponent,
+    ...canActivate(redirectUnauthorizedToLogin),
   },
 ];
