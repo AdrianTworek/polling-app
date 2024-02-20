61c34e208b5dfa77ebfe70575fa2bf7d0a5566eb
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-02-20 20:55:02 +0100

    feat: tab menu navigation in Dashboard wrapper component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { TabMenuModule } from 'primeng/tabmenu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, TabMenuModule],
   templateUrl: './dashboard.component.html',
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Polls',
+        icon: 'pi pi-chart-bar',
+        routerLink: '/dashboard/polls',
+      },
+      {
+        label: 'Settings',
+        icon: 'pi pi-wrench',
+        routerLink: '/dashboard/profile',
+      },
+    ];
+  }
+}
