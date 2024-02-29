37190614e90913106026786e31fa8aa950a8f144
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-02-29 21:54:17 +0100

    feat: add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,18 @@ import { MessageService, PrimeNGConfig } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ToastModule, NavbarComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ToastModule,
+    NavbarComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   providers: [MessageService],
 })
