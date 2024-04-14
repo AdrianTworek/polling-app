381bd35b6266459b32c828b4230a48dc7622c8d3
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-04-14 21:35:00 +0200

    feat: initial poll type distinguishing and not found case

--- src/app/dashboard/polls/poll-creator/poll-creator.component.ts
+++ src/app/dashboard/polls/poll-creator/poll-creator.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { PollType } from '../../../shared/types';
+
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-poll-creator',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './poll-creator.component.html',
 })
-export class PollCreatorComponent {}
+export class PollCreatorComponent {
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  pollType = PollType;
+  pollTypeSig = signal<PollType>(PollType.Anonymous);
+
+  constructor() {
+    this.route.queryParams.subscribe((params) => {
+      this.pollTypeSig.set(params['type']);
+    });
+  }
+
+  onGoBack() {
+    this.router.navigate(['../'], {
+      relativeTo: this.route,
+    });
+  }
+}
