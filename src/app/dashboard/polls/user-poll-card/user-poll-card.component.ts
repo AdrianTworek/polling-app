6f13bccfab8f372185d2290baeebcdf0aeac9ba6
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-07-16 16:55:43 +0200

    feat: attach poll type badge to user poll card component

--- src/app/dashboard/polls/user-poll-card/user-poll-card.component.ts
+++ src/app/dashboard/polls/user-poll-card/user-poll-card.component.ts
@@ -1,16 +1,19 @@
 import { Component, input } from '@angular/core';
-import { DatePipe } from '@angular/common';
+import { DatePipe, UpperCasePipe } from '@angular/common';
 
 import { CardWrapperComponent } from '../../../shared/components/card-wrapper/card-wrapper.component';
 import { TruncatePipe } from '../../../shared/pipes/truncate.pipe';
 import { Poll } from '../poll.model';
+import { PollType } from '../../../shared/types';
 
 @Component({
   selector: 'app-user-poll-card',
   standalone: true,
-  imports: [DatePipe, TruncatePipe, CardWrapperComponent],
+  imports: [DatePipe, TruncatePipe, UpperCasePipe, CardWrapperComponent],
   templateUrl: './user-poll-card.component.html',
 })
 export class UserPollCardComponent {
   poll = input.required<Poll>();
+
+  pollType = PollType;
 }
