f4f72dab8a2400260ab0359823cc2c46762bae59
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-09-12 14:20:15 +0200

    feat: extend poll model with votedBy field and SelectedOption type

--- src/app/dashboard/polls/poll.model.ts
+++ src/app/dashboard/polls/poll.model.ts
@@ -10,9 +10,19 @@ export interface Poll {
   createdAt: Timestamp;
   type: PollType;
   options: PollOption[];
+  votedBy: string[];
+}
+
+export interface PollCreate {
+  title: string;
+  multipleChoicesAllowed: boolean;
+  options: PollOption[];
+  type: PollType;
 }
 
 export interface PollOption {
   value: string;
   votes: number;
 }
+
+export type SelectedOption = boolean | string;
