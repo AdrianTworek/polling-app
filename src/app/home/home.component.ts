244a0e5c65aec269fb042114c21d6edef83fdbb4
Adrian Tworek <adrian.tworek.it@gmail.com>
2024-09-16 15:07:49 +0200

    feat: home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, RouterLink],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
